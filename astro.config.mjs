0df40e6a535e237d5104c82ca4faa57ec3dce482
jose sanchez <jsanchez@vailsys.com>
2023-05-11 22:40:46 -0500

    optimize css

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,9 @@ import astroI18next from "astro-i18next";
 import react from "@astrojs/react";
 
 // https://astro.build/config
+import image from "@astrojs/image";
+
+// https://astro.build/config
 export default defineConfig({
-  integrations: [react(), astroI18next()]
+  integrations: [react(), astroI18next(), image()]
 });
